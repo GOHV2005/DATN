32b9a735f273659a76412c07cf7bda7a5e7a00f2
HDuyWiBu <thanhtuu2019.hk@gmail.com>
2025-11-26 15:33:49 +0700

    MiniGame1+2 hoàn chỉnh

--- DEMEN/Assets/HDii 1/Minigame1Asset/Card.cs
+++ DEMEN/Assets/HDii 1/Minigame1Asset/Card.cs
@@ -1,51 +1,77 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
 
 public class Card : MonoBehaviour
 {
-    public int cardID;               // ID của cặp (0,1,2...)
-    public Sprite frontSprite;       // mặt trước
-    public Sprite backSprite;        // mặt sau
+    public int cardID; // ID cặp
+    public Sprite frontSprite;
+    public Sprite backSprite;
     private Image image;
     private bool isRevealed = false;
     private Button button;
+
+    [Header("Sound")]
+    public AudioClip flipSound;
+    private AudioSource audioSource;
 
     private void Awake()
     {
         image = GetComponent<Image>();
         button = GetComponent<Button>();
-        button.onClick.AddListener(OnCardClick);
+        audioSource = gameObject.AddComponent<AudioSource>();
+        image.sprite = backSprite;
+        button.onClick.AddListener(OnClick);
     }
 
-    public void Setup(Sprite front, Sprite back, int id)
+    public void OnClick()
     {
-        frontSprite = front;
-        backSprite = back;
-        cardID = id;
-        HideCard();
+        if (!isRevealed)
+        {
+            StartCoroutine(FlipCard());
+        }
     }
 
-    public void OnCardClick()
+    IEnumerator FlipCard()
     {
-        if (isRevealed || !button.interactable) return;
-        ShowCard();
-        GameManager.Instance.OnCardRevealed(this);
+        audioSource.PlayOneShot(flipSound);
+        // Scale về 0 trên trục X
+        float duration = 0.15f;
+        float elapsed = 0f;
+        while (elapsed < duration)
+        {
+            transform.localScale = new Vector3(Mathf.Lerp(1, 0, elapsed / duration), 1, 1);
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+        transform.localScale = new Vector3(0, 1, 1);
+
+        // Đổi sprite
+        image.sprite = isRevealed ? backSprite : frontSprite;
+        isRevealed = !isRevealed;
+
+        // Scale lại
+        elapsed = 0f;
+        while (elapsed < duration)
+        {
+            transform.localScale = new Vector3(Mathf.Lerp(0, 1, elapsed / duration), 1, 1);
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+        transform.localScale = new Vector3(1, 1, 1);
+
+        // Báo GameManager
+        GameManager.Instance.CardRevealed(this);
     }
 
-    public void ShowCard()
-    {
-        isRevealed = true;
-        image.sprite = frontSprite;
-    }
-
-    public void HideCard()
+    public void Hide()
     {
         isRevealed = false;
         image.sprite = backSprite;
     }
 
-    public void Disable()
+    public bool IsRevealed()
     {
-        button.interactable = false;
+        return isRevealed;
     }
 }
