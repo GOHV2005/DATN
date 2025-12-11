0f4daacb861022bc3f40d80b64f6de49cb1a85d5
Doramonaa <thanhdhps40361@gmail.com>
2025-12-12 03:04:25 +0700

    a

---
+++ DEMEN/Assets/HDii 1/Minigame1Asset/MinigameTrigger2D.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class MinigameTrigger2D : MonoBehaviour
+{
+    public string minigameName; // Minigame1 hoặc Minigame2
+    private bool playerInRange = false;
+
+    void Update()
+    {
+        if (playerInRange && Input.GetKeyDown(KeyCode.E))
+        {
+            SceneManagerHelper.Instance.GoToMinigame(minigameName);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Player vao trigger");
+        if (collision.CompareTag("Player"))
+            playerInRange = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+            playerInRange = false;
+    }
+}
